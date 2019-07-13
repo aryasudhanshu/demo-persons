b737f8cd46d2056b9ad67c27d5c57ada58dc5850
Sudhanshu Arya <aryasudhanshu@gmail.com>
2019-07-13 17:26:01 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi! I am a React App</h1>
+      <Person name="Max" age="28"/>
+      <Person name="Manu" age="29">My Hobbies: Racing</Person>
+      <Person name="Stephanie" age="26"/>
     </div>
   );
 }
